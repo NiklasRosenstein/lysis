d70fd13f79ac2d3982c4f5e2cd94fb189dc36a5a
Niklas Rosenstein <rosensteinniklas@gmail.com>
2013-10-16 12:12:08 +0200

    added support for multiple expressions

--- lysis/__main__.py
+++ lysis/__main__.py
@@ -33,45 +33,41 @@ def main():
     argp = argparse.ArgumentParser(description='Evaluate propositional '
             'calculus and set expressions (latter has yet to be '
             'implemented).')
-    argp.add_argument('expr', help='The expression to evaluate.')
+    argp.add_argument('expr', nargs='+', help='One or more expressions to '
+            'evaluate.')
     argp.add_argument('-t', '--table', help='Evaluate all possible '
             'combinations of the variables and print out a table. Only valid '
             'for propositional calculus.', action='store_true')
     argp.add_argument('-nw', '--no-warn', help='Disables the warning if '
             'expected output/calculation time exceeds a certain number.',
             action='store_true')
-    argp.add_argument('-d', '--debug', help='Print debug information (eg. '
-            'the variables that have been found and a re-stringified '
-            'representation of the expression.', action='store_true')
     args = argp.parse_args()
 
-    # Parse the expression.
+    # Parse the expressions.
     parser = lysis.parser.Parser()
-    try:
-        node, varset = parser.parse(args.expr)
-    except lysis.error.SyntaxError as exc:
-        print "[SyntaxError]:", exc
-        return 1
+    nodes = []
+    variables = set()
+    for expr in args.expr:
+        try:
+            node, varset = parser.parse(expr)
+            variables |= varset
+            nodes.append((node, varset))
+        except lysis.error.SyntaxError as exc:
+            print "[SyntaxError]:", exc
+            return 1
 
-    # Print debug stuff?
-    if args.debug:
-        print
-        print "Expression: ", node
-        print "Variables:  ", varset
-        print
+    # Sort the variables into a fixed tuple.
+    variables = tuple(sorted(variables))
 
     # Print a table?
     if args.table and not args.no_warn:
-        n = 2 ** len(varset)
+        n = 2 ** len(variables)
         if n > 50:
             answer = raw_input('There are %d possibilities, do you really '
                                'want to print the full table? [y/n]  ' % n)
             if answer.strip().lower() not in ('y', 'yes', 'true'):
                 args.table = False
     if args.table:
-        # Create a sorted list of the variables.
-        variables = list(sorted(varset))
-
         # Generate the head-line and under-line.
         headline = ''
         underline = ''
@@ -81,10 +77,10 @@ def main():
             if i < len(variables) - 1:
                 headline += ' | '
                 underline += '-+-'
-
-        strformat = str(node)
-        headline += ' | ' + strformat
-        underline += '-+-' + len(strformat) * '-'
+        for node, varset in nodes:
+            strformat = str(node)
+            headline += ' | ' + strformat
+            underline += '-+-' + len(strformat) * '-'
 
         print headline
         print underline
@@ -96,8 +92,8 @@ def main():
                 line += fmt_bool(context.get(var))
                 if i < len(variables) - 1:
                     line += ' | '
-
-            line += ' | ' + fmt_bool(node.evaluate(context))
+            for node, varset in nodes:
+                line += ' | ' + fmt_bool(node.evaluate(context)) + (len(str(node)) - 1) * ' '
 
             print line
 
